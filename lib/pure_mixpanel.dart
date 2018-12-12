678f9ab4e1c51b41ea19dec88b6f38a901ca5bb5
Victor Choueiri <victor@ctrlanddev.com>
2018-12-12 15:01:21 +0100

    Add trailing commas, reformat

--- lib/pure_mixpanel.dart
+++ lib/pure_mixpanel.dart
@@ -16,33 +16,41 @@ class Mixpanel {
 
   Mixpanel({@required this.token, this.debug: false, this.trackIp: false});
 
-  Future<http.Response> track(String eventName,
-      {String distinctID, Map<String, String> properties}) async {
+  Future<http.Response> track(
+    String eventName, {
+    String distinctID,
+    Map<String, String> properties,
+  }) async {
     if (properties == null) {
       properties = Map<String, String>();
     }
     properties.putIfAbsent('token', () => this.token);
 
     var payload = MixpanelPayload.createEncoded(
-        event: eventName, distinctID: distinctID, properties: properties);
+      event: eventName,
+      distinctID: distinctID,
+      properties: properties,
+    );
     final uri = MixpanelUri.create(path: '/track', queryParameters: {
       'data': payload,
       'verbose': (this.debug ? '1' : '0'),
-      'ip': (this.trackIp ? '1': '0'),
+      'ip': (this.trackIp ? '1' : '0'),
     });
     if (debug) {
       print(
-          'mixpanel req\n\tproperties: $properties\n\turi: ${uri.toString()}');
+        'mixpanel req\n\tproperties: $properties\n\turi: ${uri.toString()}',
+      );
     }
     return http.get(uri.toString());
   }
 }
 
 class MixpanelPayload {
-  static String createEncoded(
-      {@required String event,
+  static String createEncoded({
+    @required String event,
       String distinctID,
-      Map<String, String> properties}) {
+    Map<String, String> properties,
+  }) {
     if (distinctID != null) {
       properties.putIfAbsent('distinct_id', () => distinctID);
     }
@@ -63,8 +71,10 @@ class MixpanelPayload {
 }
 
 class MixpanelUri {
-  static Uri create(
-      {@required String path, @required Map<String, dynamic> queryParameters}) {
+  static Uri create({
+    @required String path,
+    @required Map<String, dynamic> queryParameters,
+  }) {
     return Uri(
       scheme: 'http',
       host: MixpanelBaseUri,
